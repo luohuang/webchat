3038a2cc8f8f5357c20aad83bcc01c8e65dfdc2b
luohuang <luohuang0423@qq.com>
2015-07-06 22:18:20 +0800

    index.js
    The server point file using javdScript.

---
+++ index.js
@@ -0,0 +1,24 @@
+// 2015-7-6 write a index.js to be a router file
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+	});
+
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+	});
+});
+
+http.listen(3000, function(){
+	console.log('listening on *: 3000');
+});
